9ff8aee1fdca51778d440d67d661ebe9edde22b8
Lukasz Niepala <luka0304sz@gmail.com>
2019-06-06 10:35:24 +0200

    first commit

--- Src/main.c
+++ Src/main.c
@@ -30,7 +30,6 @@
 /* USER CODE BEGIN Includes */
 #include "BMPXX80.h"
 #include "delays.h"
-#include "bmp280.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -53,7 +52,6 @@
 /* USER CODE BEGIN PV */
 float temperature, huminidity;
 int32_t pressure;
-BMP280_HandleTypedef bmp280;
 
 //float pressure, temperature, humidity;
 
@@ -115,21 +113,7 @@ int main(void)
   test=10;
   BME280_SetConfig(BME280_STANDBY_MS_10, BME280_FILTER_OFF);
 
-//  bmp280_init_default_params(&bmp280.params);
-//  	bmp280.addr = BMP280_I2C_ADDRESS_0;
-//  	bmp280.i2c = &hi2c1;
-//
-//  	while (!bmp280_init(&bmp280, &bmp280.params)) {
-//  		size = sprintf((char *)Data, "BMP280 initialization failed\n");
-//  		test++;
-//
-////  		HAL_UART_Transmit(&huart1, Data, size, 1000);
-//  		HAL_Delay(2000);
-//  	}
-//  	bool bme280p = bmp280.id == BME280_CHIP_ID;
-//  	size = sprintf((char *)Data, "BMP280: found %s\n", bme280p ? "BME280" : "BMP280");
-//  	test=69;
-////  	HAL_UART_Transmit(&huart1, Data, size, 1000);
+
 
   /* USER CODE END 2 */
 
@@ -140,32 +124,11 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  test++;
+
 	  	BME280_ReadTemperatureAndPressureAndHuminidity(&temperature, &pressure, &huminidity);
-//	  	HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-////	  	HAL_Delay(2000);
-//	  	HAL_Delay(100);
-//	  			while (!bmp280_read_float(&bmp280, &temperature, &pressure, &humidity)) {
-//	  				size = sprintf((char *)Data,
-//	  						"Temperature/pressure reading failed\n");
-////	  				HAL_UART_Transmit(&huart1, Data, size, 1000);
-//	  				HAL_Delay(2000);
-//	  			}
 //
-//	  			size = sprintf((char *)Data,"Pressure: %.2f Pa, Temperature: %.2f C",
-//	  					pressure, temperature);
-////	  			HAL_UART_Transmit(&huart1, Data, size, 1000);
-//	  			if (bme280p) {
-//	  				size = sprintf((char *)Data,", Humidity: %.2f\n", humidity);
-////	  				HAL_UART_Transmit(&huart1, Data, size, 1000);
-//	  			}
-//
-//	  			else {
-//	  				size = sprintf((char *)Data, "\n");
-////	  				HAL_UART_Transmit(&huart1, Data, size, 1000);
-//	  			}
-	  			HAL_Delay(2000);
-	  			test=19;
+	  	HAL_Delay(2000);
+	  	test=19;
   }
 
   /* USER CODE END 3 */
